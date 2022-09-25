ee13b3662bba27ef412ca1f182c34f144e2e49bb
psalma20 <98271452+psalma20@users.noreply.github.com>
2022-09-25 22:14:53 +0800

    Mando changes

--- CART.php
+++ CART.php
@@ -2,11 +2,8 @@
 //start session
 session_start();
 
-
-
 require_once("component.php");
 require_once("CreateDb.php");
-
 
 //create instance of Createdbclass
 $database = new CreateDb("Productdb", "Producttb");
@@ -41,34 +38,22 @@ if (isset($_POST['add'])){
         print_r($_SESSION['cart']);
     }
 }
-
-
-
-
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
 	<title>Courses</title>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" rel="stylesheet">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" rel="stylesheet">
 	<link href="style2.css" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <!-- Bootstrap CSS only -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <<link href="bootstrap.css" rel="stylesheet">
 <!--Font awesome-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" integrity="sha384-X8QTME3FCg1DLb58++lPvsjbQoCT9bp3MsUU3grbIny/3ZwUJkRNO8NPW6zqzuW9" crossorigin="anonymous">
 
 </head>
 <body>
-
 
 <?php require_once ("header.php"); ?>
 <div class="container">
@@ -81,11 +66,6 @@ if (isset($_POST['add'])){
    
     ?>
     </div>
-
-
-
-
-
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- header.php
+++ header.php
@@ -1,41 +1,35 @@
-<header id="header">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="CART.php" class="navbar-brand">
-            <h3 class="px-5">
-                <i class="fas fa-shopping-basket"></i> Shopping Cart
-            </h3>
-        </a>
-        <button class="navbar-toggler"
-            type="button"
-                data-toggle="collapse"
-                data-target = "#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="mr-auto"></div>
-            <div class="navbar-nav">
-                <a href="cart1.php" class="nav-item nav-link active">
-                    <h5 class="px-5 cart">
-                        <i class="fas fa-shopping-cart"></i> Cart
-                        <?php
-
-                        if (isset($_SESSION['cart'])){
-                            $count = count($_SESSION['cart']);
-                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
-                        }else{
-                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
-                        }
-
-                        ?>
-                    </h5>
-                </a>
-            </div>
-        </div>
-
-    </nav>
-</header>
+<aside aria-label="Slider Area">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>About us</title>
+        <link href="courses.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <input type ="checkbox" name ="" id ="chk1">
+            <div class="logo"><h1><img src="logo.png" width="100px" style="background:none;" ></h1></div>
+                <ul>
+                    <li><a href="C:/xampp/htdocs/Heisenberg-Empire/index.html">Home</a></li>
+                    <li><a href="#Courses.html">Courses</a></li>
+                    <li><a href="#About Us.html">About Us</a></li>
+                    <li><a href="#Profile.html">Profile</a></li>
+                    <li><a href="#Contact.html">Contact</a></li>
+					<div class="navbar-nav">
+                		<a href="cart1.php" class="nav-item nav-link active">
+							<h5 class="px-5 cart">
+								<i class="fas fa-shopping-cart"></i> Cart
+								<?php
+								if (isset($_SESSION['cart'])){
+									$count = count($_SESSION['cart']);
+									echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+								}else{
+									echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+								}
+								?>
+							</h5>
+               			</a>
+            		</div>
+                </ul>
+    </body>
+</aside>
